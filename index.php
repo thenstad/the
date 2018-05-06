226cac82c5a110fd9224c4c3a23a202789244a00
Thomas <thomas.holten@yahoo.no>
2018-05-06 07:46:39 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>the</title>
+
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	</head>
+	<body>
+
+	<div class="top">
+		<div class="b1">b1</div>
+	</div>
+
+	<div class="mid">
+		<div class="b2">b2</div>
+		<div class="b3">b3</div>
+	</div>
+
+	<div class="bot">
+		<div class="b3">b3</div>
+	</div>
+
+	</body>
+</html>
